40c3fcc6e91d20a2ea80808fd60e99d6be0918f0
Tayyib Esau <esamu161@gmail.com>
2020-11-12 11:23:59 +1300

    updated css

--- src/App.js
+++ src/App.js
@@ -1,63 +1,10 @@
-const DATA = [
-  {
-    letter: "Q",
-    keycode: 81,
-    id: "Open-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-  },
-  {
-    letter: "W",
-    keycode: 87,
-    id: "Closed-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-  },
-  {
-    letter: "E",
-    keycode: 69,
-    id: "Kick-and-Hat",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-  },
-  {
-    letter: "A",
-    keycode: 65,
-    id: "Punchy-Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
-  },
-  {
-    letter: "S",
-    keycode: 83,
-    id: "Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-  },
-  {
-    letter: "D",
-    keycode: 68,
-    id: "Snare",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
-  },
-  {
-    letter: "Z",
-    keycode: 90,
-    id: "Side-Stick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
-  },
-  {
-    letter: "X",
-    keycode: 88,
-    id: "Clap",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-  },
-  {
-    letter: "C",
-    keycode: 67,
-    id: "Shaker",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
-  },
-];
+import React from "react";
+import "./App.css";
+import DATA from "./data.js";
 
 const App = () => {
   const [displayInfo, setDisplayInfo] = React.useState("Lets Play!");
-  const [power, setPower] = React.useState(true);
+  const [power, setPower] = React.useState(false);
   const keys = React.useState(DATA)[0];
 
   const play = (id) => {
@@ -84,16 +31,25 @@ const App = () => {
 
   return (
     <div id="drum-machine">
-      <div id="display">{power ? displayInfo : "Power Off."}</div>
+      <div id="display" style={{ backgroundColor: !power ? "grey" : null }}>
+        {power ? displayInfo : "Power Off."}
+      </div>
+      {/*
+        <div id="button">
+          <button onClick={() => {
+              setPower(!power);
+              if (power) setDisplayInfo("Lets Play!")
+            }}>Power</button>
+        </div>  */}
       <div id="button">
-        <button
+        <input
+          className="l"
+          type="checkbox"
           onClick={() => {
             setPower(!power);
             if (power) setDisplayInfo("Lets Play!");
           }}
-        >
-          Power
-        </button>
+        />
       </div>
       <div id="drum-pads">{power ? enabledButtons : disabledButtons}</div>
     </div>
@@ -101,42 +57,46 @@ const App = () => {
 };
 
 const Box = ({ id, letter, play, audioLink }) => {
-  let audio = new Audio(audioLink);
+  //let audio = new Audio(audioLink);
   return (
-    <div
-      className="drum-pad"
-      id={id}
-      onClick={() => {
-        // audio.play();
-        //play(id);
-        // This is just to pass the fcc tests
-        // The 2 lines commented out above functionally work fine in replacement
-        // tests need to find correct id etc so just gets confusing for no reason
-        const audio = document.getElementById(letter);
-        if (audio) {
-          audio.currentTime = 0;
-          audio.play();
-          const dataObject = DATA.filter((i) => i.letter === letter)[0].id;
-          document.getElementById("display").innerText = dataObject;
-        }
-      }}
-    >
-      <audio
-        ref={(ref) => (this.audio = ref)}
-        src={audioLink}
-        className="clip"
-        id={letter}
-      />
-      {letter}
-    </div>
+    <>
+      <div
+        className="button"
+        id={id}
+        onClick={() => {
+          // audio.play();
+          //play(id);
+          // This is just to pass the fcc tests
+          // The 2 lines commented out above functionally work fine in replacement
+          // tests need to find correct id etc so just gets confusing for no reason
+          const audio = document.getElementById(letter);
+          if (audio) {
+            audio.currentTime = 0;
+            audio.play();
+            const dataObject = DATA.filter((i) => i.letter === letter)[0].id;
+            document.getElementById("display").innerText = dataObject;
+          }
+        }}
+      >
+        <button className="drum-pad" id={id}>
+          <audio
+            ref={(ref) => (this.audio = ref)}
+            src={audioLink}
+            className="clip"
+            id={letter}
+          />
+          {letter}
+        </button>
+      </div>
+    </>
   );
 };
-
+// Disabled Button Component
 const BlankBox = ({ id, letter }) => {
   return (
-    <div className="drum-pad" id={id}>
-      {letter}
-    </div>
+    <button className="btn">
+      <span>{letter}</span>
+    </button>
   );
 };
 
@@ -155,4 +115,4 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
